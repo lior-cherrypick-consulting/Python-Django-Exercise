5a635aecd46102a642edee0231f189cbc7b64ab7
Lior Ashkenazi <lior@cherrypick-consulting.com>
2024-03-25 10:17:58 +0200

    Added pagination for posts

--- blog_platform/blog/views/post_listview.py
+++ blog_platform/blog/views/post_listview.py
@@ -1,12 +1,16 @@
+from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from blog.models import Post
 from blog.serializers import PostSerializer
 from blog.filters import PostFilter
 
+class PostListViewPagination(PageNumberPagination):
+    page_size = 10
 
 class PostListView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PostFilter
+    pagination_class = PostListViewPagination
