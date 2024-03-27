8b117db93bff625640b611acad692fae788831b4
Lior Ashkenazi <lior@cherrypick-consulting.com>
2024-03-27 17:38:52 +0200

    Added support for CORS

--- blog_platform/blog_platform/settings.py
+++ blog_platform/blog_platform/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "blog_platform.urls"
